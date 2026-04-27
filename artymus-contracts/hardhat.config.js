d89051ae7f4f365e60f0f98078745cabf52191d1
TheBluCog <george@thedunphys.ca>
2026-04-26 20:48:08 -0400

    Add Hardhat config

---
+++ artymus-contracts/hardhat.config.js
@@ -0,0 +1,18 @@
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  solidity: '0.8.20',
+  networks: {
+    polygon: {
+      url: process.env.POLYGON_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    polygonAmoy: {
+      url: process.env.AMOY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
