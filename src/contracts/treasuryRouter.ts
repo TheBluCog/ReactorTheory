4efa2cf4a2b19116118f530909d414a38e41fb69
TheBluCog <george@thedunphys.ca>
2026-05-02 02:33:40 -0400

    Add TreasuryRouter ABI for distribute call

---
+++ src/contracts/treasuryRouter.ts
@@ -0,0 +1,12 @@
+export const TREASURY_ROUTER_ABI = [
+  {
+    name: 'distribute',
+    type: 'function',
+    stateMutability: 'nonpayable',
+    inputs: [
+      { name: 'recipients', type: 'address[]' },
+      { name: 'amounts', type: 'uint256[]' },
+    ],
+    outputs: [],
+  },
+] as const
